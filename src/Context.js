de948eaf22ccfc63bb994d54ea789575e208cbfb
hankliu5 <yliu224@ncsu.edu>
2018-05-27 15:39:44 -0400

    added marked to support markdown language.

--- src/Context.js
+++ src/Context.js
@@ -1,24 +1,17 @@
 import React, { Component } from 'react';
+import marked from 'marked';
+
 import "./Context.css"
 
 class Context extends Component {
+
+    getMarkdownText() {
+        var rawMarkup = marked('# This is _Markdown_.', { sanitize: true });
+        return { __html: rawMarkup };
+    }
     render() {
         var visibility = this.props.menuVisibility ? "show" : "hide"
-
-        return(
-            <div id="context" className={visibility}>
-                <p>Can you spot the item that doesn't belong?</p>
-                <ul>
-                    <li>Lorem</li>
-                    <li>Ipsum</li>
-                    <li>Dolor</li>
-                    <li>Sit</li>
-                    <li>Bumblebees</li>
-                    <li>Aenean</li>
-                    <li>Consectetur</li>
-                </ul>
-            </div>
-        );
+        return <div id="context" className={visibility} dangerouslySetInnerHTML={this.getMarkdownText()} />
     }
 }
 
