5a45067f914f721c5ac2c1a9fed26d12a8617b11
hankliu5 <yliu224@ncsu.edu>
2018-05-27 15:54:15 -0400

    read markdown text from file.

--- src/Context.js
+++ src/Context.js
@@ -5,13 +5,37 @@ import "./Context.css"
 
 class Context extends Component {
 
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+            markdown: null
+        };
+    }
+
+    componentWillMount() {
+        const readmePath = require("./Readme.md");
+
+        fetch(readmePath)
+            .then(response => {
+                return response.text();
+            })
+            .then(text => {
+                this.setState({
+                    markdown: marked(text, { sanitize: true })
+                })
+            })
+    }
+
     getMarkdownText() {
         var rawMarkup = marked('# This is _Markdown_.', { sanitize: true });
         return { __html: rawMarkup };
     }
+
     render() {
         var visibility = this.props.menuVisibility ? "show" : "hide"
-        return <div id="context" className={visibility} dangerouslySetInnerHTML={this.getMarkdownText()} />
+        const { markdown } = this.state
+        return <div id="context" className={visibility} dangerouslySetInnerHTML={{ __html: markdown }} />
     }
 }
 
