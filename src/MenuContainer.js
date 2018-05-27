e4138698978b4a2e42c12c9a6a19a02761005aca
hankliu5 <yliu224@ncsu.edu>
2018-05-27 15:06:25 -0400

    adjust layout.

--- src/MenuContainer.js
+++ src/MenuContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import MenuButton from "./MenuButton";
 import Menu from "./Menu";
+import Context from "./Context";
 import "./MenuContainer.css"
 
 
@@ -38,24 +39,13 @@ class MenuContainer extends Component {
     render() {
         var visibility = this.state.visible ? "hide" : "show"
         return (
-            <div class="row">
+            <div className="row">
                 <Menu handleMouseDown={this.handleMouseDown} menuVisibility={this.state.visible} />
-                <div class="column left">
+                <div className="column left">
                     <MenuButton handleMouseDown={this.handleMouseDown} />
                 </div>
-                <div class="column right">
-                    <div id="flyoutContext" onMouseDown={this.handleMouseDown} className={visibility}>
-                        <p>Can you spot the item that doesn't belong?</p>
-                        <ul>
-                            <li>Lorem</li>
-                            <li>Ipsum</li>
-                            <li>Dolor</li>
-                            <li>Sit</li>
-                            <li>Bumblebees</li>
-                            <li>Aenean</li>
-                            <li>Consectetur</li>
-                        </ul>
-                    </div>
+                <div className="column right">
+                    <Context menuVisibility={this.state.visible} />
                 </div>
             </div>
         );
